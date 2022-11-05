ea3deecf98993edab52fef6dfe9733ad3133183a
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-11-04 22:24:28 -0300

    [04/11/2022] Tabuada.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Tabuada.");
+Console.Write("Tabuada do número: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+Console.Write("Escolha um número de vezes para multiplicar: ");
+int vezes = Convert.ToInt32(Console.ReadLine());
+int i2 = 0;
+while (i2 <= vezes)
+{
+Console.Write($"{numero} X {i2} = {numero*i2}\n");
+i2 = i2 + 1;
+}
