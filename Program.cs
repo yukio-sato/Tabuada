55d177ea77e5ec81a03d686c03452ac78b2e9a47
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-11-07 20:01:46 -0300

    [07/11/2022] Update de colorização das letras, e alguns errors ortografico arrumado.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,38 @@
-﻿Console.Clear();
-Console.WriteLine("Tabuada.");
-Console.Write("Tabuada do número: ");
+﻿Console.ForegroundColor = ConsoleColor.Yellow;
+string frase = "- - - Tabuada - - -\n";
+for (int i = 0; i < frase.Length; i++)
+{
+    Console.Write(frase[i]);
+    Thread.Sleep(55);
+}
+Console.ForegroundColor = ConsoleColor.White;
+frase = "Escolha um número para fazer tabuada do: ";
+for (int i = 0; i < frase.Length; i++)
+{
+    Console.Write(frase[i]);
+    Thread.Sleep(55);
+}
+Console.ForegroundColor = ConsoleColor.Cyan;
 int numero = Convert.ToInt32(Console.ReadLine());
-Console.Write("Escolha um número de vezes para multiplicar: ");
+Console.ForegroundColor = ConsoleColor.White;
+frase = "Escolha um número de vezes: ";
+for (int i = 0; i < frase.Length; i++)
+{
+    Console.Write(frase[i]);
+    Thread.Sleep(55);
+}
+Console.ForegroundColor = ConsoleColor.Cyan;
 int vezes = Convert.ToInt32(Console.ReadLine());
 int i2 = 0;
 while (i2 <= vezes)
 {
-Console.Write($"{numero} X {i2} = {numero*i2}\n");
+    Console.ForegroundColor = ConsoleColor.Blue;
+frase = $"{numero} X {i2} = {numero*i2}\n";
+for (int i = 0; i < frase.Length; i++)
+{
+    Console.Write(frase[i]);
+    Thread.Sleep(55);
+}
 i2 = i2 + 1;
 }
+Console.ResetColor();
